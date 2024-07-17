dc72660e8f5b4cf244bbad6fb4fbea0d9611ee97
kindywu <kindywu@outlook.com>
2024-07-17 17:07:11 +0800

    commit

--- examples/spawn_blocking.rs
+++ examples/spawn_blocking.rs
@@ -3,19 +3,34 @@ use std::thread;
 use tokio::runtime::Runtime;
 
 fn main() {
-    let rt = Runtime::new().unwrap();
+    println!("thread_id: {:?}", thread::current().id());
+
+    let mut handles = Vec::new();
+
+    for i in 0..3 {
+        let rt = Runtime::new().unwrap();
+        let handle = thread::Builder::new()
+            .name(format!("ReplBackend {i}"))
+            .spawn(move || {
+                let ret = rt.block_on(async {
+                    println!("thread_id: {:?}", thread::current().id());
+
+                    rt.spawn_blocking(|| {
+                        println!("thread_id: {:?}", thread::current().id());
+                        100
+                    })
+                    .await
+                });
+                println!("thread_id: {:?} {:?}", thread::current().id(), ret)
+            })
+            .unwrap();
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
     println!("thread_id: {:?}", thread::current().id());
-    let ret = rt.block_on(async {
-        println!("thread_id: {:?}", thread::current().id());
-
-        rt.spawn_blocking(|| {
-            println!("thread_id: {:?}", thread::current().id());
-            100
-        })
-        .await
-    });
-
-    println!("thread_id: {:?} {:?}", thread::current().id(), ret)
 }
 // Create the runtime
